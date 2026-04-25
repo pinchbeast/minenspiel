dcc2bddae2d4096637453a4e8276939d92c724b1
Josias Grischott <josias.grischott@ikmail.com>
2026-04-26 01:46:43 +0200

    now its a window with a variable amount of buttons which print their location when pressed. some progress but still a long way i assume

--- minenspiel.py
+++ minenspiel.py
@@ -1,14 +1,45 @@
 import tkinter as tk
 
+
+
+def merda(h,w):
+
+    print("button mit koordinaten: ", h, w, "wurde gedrückt")
+
+
 root = tk.Tk()
-
-
+#here change size of the grid
+size = 12
 root.title("Minenspiel")
 root.configure(background="white")
 root.minsize(500, 500)
 root.maxsize(1000, 1000)
 
-label1 = tk.Label(root, text="merda").pack()
+counter = 0
+array = ([])
+
+#create array from 1-size
+while(counter < size):
+    array.append(counter)
+    counter = counter + 1
+
+
+# x = row, counter = column
+
+while(counter > 0):
+    for x in array:
+        buttonname = (f"button_{x}_{counter}")
+        buttonname = tk.Button(root, text="o", width=1, height=1, command=lambda r=counter, c=x: merda(r,c))
+        buttonname.grid(row=x, column=counter)
+        
+
+
+    counter = counter - 1    
+
+
+
+#b1a = tk.Button(root, text="o", width=2, height=2, command=lambda: merda(1,1)).pack()
+
 
 
 
