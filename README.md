f697336b0430e303357fe02efc5b698c86e636da
Josias Grischott <josias.grischott@ikmail.com>
2026-04-25 17:29:00 +0200

    First commit. Not playable yet

---
+++ README.md
@@ -0,0 +1,10 @@
+Please make sure you have Python 3.x and tkinter installed
+I used Python 3.14.4
+
+For arch:
+sudo pacman -S python tk
+
+For ubuntu/debian(not tested):
+sudo apt-get install python python3-tk
+
+
---
+++ minenspiel.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+
+root = tk.Tk()
+
+
+root.title("Minenspiel")
+root.configure(background="white")
+root.minsize(500, 500)
+root.maxsize(1000, 1000)
+
+label1 = tk.Label(root, text="merda").pack()
+
+
+
+
+root.mainloop()
