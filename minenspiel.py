f966ee990e3b3b69800cfa36e40e72e496ea3565
Josias Grischott <josias.grischott@ikmail.com>
2026-04-27 01:30:50 +0200

    now everytime you pop a field the neighbours get poped to and it shows the number of how many mines are nearby. but the neighbours dont do that so far

--- minenspiel.py
+++ minenspiel.py
@@ -6,35 +6,55 @@ import random
 def merda(h,w, btn):
 
     print("button mit koordinaten: x", h,"y", w, "wurde gedrückt")
-    guesscode = size * (w) + (h+1)
+    guesscode = size * w + h
     if(catalog[guesscode] == 0):
         print("guter guess")
-        btn.config(text="O")
+        
+        minenumbernearby=nearby(guesscode, h, w)
+        btn.config(text=minenumbernearby)
     elif(catalog[guesscode] == 69):
         print("ALARM VERKACKT DU DEPP")
         btn.config(text="X", background="red")
 
+def nearby(orig, x,y):
+    codes = [orig + 1, orig-1,orig+size,orig-size,orig + 1 + size, orig-1 + size,orig-size +1,orig-size-1]
+    #codes.append([orig + 1], [orig-1],[orig+size],[orig-size])
+    minenumbernearby = 0
+
+    #this for tests every one of the nearby fields
+    for nearfield in codes:
+
+        if(catalog[nearfield] == 0):
+            print("guter guess")
+            buttons[nearfield].config(text="O")
+            tocheck.append(nearfield)
+        elif(catalog[nearfield] == 69):
+            print("ALARM VERKACKT DU DEPP")
+            minenumbernearby = minenumbernearby + 1
+    return minenumbernearby
 
 
 root = tk.Tk()
 #here change size of the grid and set mines
-size = 4
-mines = 5
+size = 12
+mines = 80
 root.title("Minenspiel")
 root.configure(background="white")
 root.minsize(500, 500)
 root.maxsize(1000, 1000)
 
 
-
-# x = row, counter = column
+tocheck = ([])
+buttons = {}
 
 for y in range(0, size):
     for x in range(0, size):
-        buttonname = (f"button_{x}_{y}")
+        guesscode = size * x + y
+        buttonname = (f"button_{guesscode}")
         buttonname = tk.Button(root, text="", width=1, height=1)
         buttonname.config(command=lambda r=y, c=x, b=buttonname: merda(r,c,b))
         buttonname.grid(row=x, column=y)
+        buttons[guesscode]=buttonname
  
 ranger = size ** 2 + 1
 catalog = [0] * ranger
